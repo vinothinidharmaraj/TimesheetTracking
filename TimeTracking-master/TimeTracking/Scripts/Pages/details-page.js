b5446a677fe1833d14529498ba64f872e33eb5f7
vinothini dharmaraj <vinothini@zen3.com>
2017-12-18 15:47:44 -0800

    adding date range

--- TimeTracking-master/TimeTracking/Scripts/Pages/details-page.js
+++ TimeTracking-master/TimeTracking/Scripts/Pages/details-page.js
@@ -4,17 +4,63 @@
     });
 
     var options = [{ "Id": 1, "Period": "Day" }, { "Id": 2, "Period": "Week" }];
-   /* options.forEach(function (option) {
-        var option = $('<option>').attr('value', this.key).html(this.value);
-        $('#period').append(option);
-    });*/
+    /* options.forEach(function (option) {
+         var option = $('<option>').attr('value', this.key).html(this.value);
+         $('#period').append(option);
+     });*/
     $.each(options, function () {
         var option = $('<option>').attr('value', this.Id).html(this.Period);
+
         $('#period').append(option);
+
+        var today = getCurrentDate();
+        $("#fromdatepicker").val(today);
+        //$("#fromdatepicker").text(today);
+        $("#todatepicker").val(today);
+        //$("#todatepicker").text(today);
     });
 
+    $("#todatepicker").on("change", function () {
+        var todate = new Date(this.value);
+        var fromdate = new Date($("#fromdatepicker")[0].value);
+    });
+    // Returns an array of dates between the two dates
+    var getDates = function (startDate, endDate) {
+        var dates = [],
+            currentDate = startDate,
+            addDays = function (days) {
+                var date = new Date(this.valueOf());
+                date.setDate(date.getDate() + days);
+                return date;
+            };
+        while (currentDate <= endDate) {
+            dates.push(currentDate);
+            currentDate = addDays.call(currentDate, 1);
+        }
+        return dates;
+    };
+
+    function getCurrentDate() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        var today = dd + '/' + mm + '/' + yyyy;
+        return today;
+    }
+
+    function generateTable() {
+
+    }
     $(function () {
         //  $("#fromdatepicker").datepicker();
-       // $("#todatepicker").datepicker();
+        // $("#todatepicker").datepicker();
     });
 });
